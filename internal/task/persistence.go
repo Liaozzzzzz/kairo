45ca76185b28fba0ec38e3b6f3612016e93ab67e
liaozzzzzz <l996712856@gmail.com>
2026-02-13 22:33:09 +0800

    fix: 更换sqlite 库

--- internal/task/persistence.go
+++ internal/task/persistence.go
@@ -13,7 +13,7 @@ import (
 	"Kairo/internal/models"
 	"Kairo/internal/utils"
 
-	_ "github.com/mattn/go-sqlite3"
+	_ "modernc.org/sqlite"
 )
 
 func (m *Manager) initDB() {
@@ -23,7 +23,7 @@ func (m *Manager) initDB() {
 		return
 	}
 	dbPath := filepath.Join(appDir, "tasks.db")
-	db, err := sql.Open("sqlite3", dbPath)
+	db, err := sql.Open("sqlite", dbPath)
 	if err != nil {
 		fmt.Printf("Failed to open database: %v\n", err)
 		return
